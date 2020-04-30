8e71b283b7d170f2a3ec62fa250fa1828fd287ab
Charles Pereira <charlesti.ptu@gmail.com>
2020-04-30 14:07:02 -0300

    reactJestTest - Get, Post

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+
+      console.log(repositories);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repository ${Date.now()}`,
+      url: 'https://mail.google.com/mail/u/0/#inbox',
+      techs: 'JavaScript, CSS, HTML'
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`, {
+      params: {
+        id,
+      }
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repos =>
+          <li key={repos.id}>
+            {repos.title}
+            <button onClick={() => handleRemoveRepository(repos.id)}>
+              Remover
           </button>
-        </li>
+          </li>)}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
